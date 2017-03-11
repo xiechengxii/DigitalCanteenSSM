f37c789099d12ccc6d1e1ddbe458ac4b4787f111
yuliu <liuyujie1994@126.com>
2017-03-11 16:51:49 +0800

    添加测试了预制菜品的分页功能

--- src/digitalCanteenSSM/controller/DishPresetController.java
+++ src/digitalCanteenSSM/controller/DishPresetController.java
@@ -1,10 +1,18 @@
 package digitalCanteenSSM.controller;
 
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.github.miemiedev.mybatis.paginator.domain.Order;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 
 import digitalCanteenSSM.po.DishPreset;
 import digitalCanteenSSM.service.DishPresetService;
@@ -26,11 +34,32 @@ public class DishPresetController {
 	//查找所有预置菜品
 	//返回菜品预置页面显示
 	@RequestMapping ("/findAllDishPreset")
-	public ModelAndView findAllDishPreset() throws Exception{
+	public ModelAndView findAllDishPreset(HttpServletRequest request) throws Exception{
+		//本段代码运行时优先从request中读取的页码和单页内容数量，
+		//如果请求从其它controller发出，并无这两个对象，
+		//则使用的是默认的值
+		String pageNum = request.getParameter("pageNum");
+		String pageSize = request.getParameter("pageSize");
+		int num = 1;
+		int size = 10;
+		if (pageNum != null && !"".equals(pageNum)) {
+			num = Integer.parseInt(pageNum);
+		}
+		if (pageSize != null && !"".equals(pageSize)) {
+			size = Integer.parseInt(pageSize);
+		}
+		
+		//配置pagehelper的排序及分页
+		String sortString = "id.desc";
+		Order.formString(sortString);
+		PageHelper.startPage(num, size);
+		
+		List<DishPreset> dishPresetList = dishPresetService.findAllDishPreset();
+		PageInfo<DishPreset> pagehelper = new PageInfo<DishPreset>(dishPresetList);
 		
 		ModelAndView modelAndView = new ModelAndView();
 		
-		modelAndView.addObject("dishPersetList",dishPresetService.findAllDishPreset());
+		modelAndView.addObject("pagehelper", pagehelper);
 		modelAndView.setViewName("/WEB-INF/jsp/dishPreset.jsp");
 		
 		return modelAndView;
